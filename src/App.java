3d5f652fa4da1b08ecb095e318b6173cfe7dd9e0
ttffkk <tfkaminski@gmail.com>
2023-06-29 00:30:22 +0200

    added Printer

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
+    public int[][] arr;
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+    
+    public static void printer(){
+        for(int z=0;z<arr.size;z++){
+            for(int y=0;y<arr[z].size;y++){
+                System.out.print(arr[z][y]);
+            }
+            System.out.println();
+        }
     }
 }
